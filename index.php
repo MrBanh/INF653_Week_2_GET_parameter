650f73f87f790ab5eb0059f4f81ae3e2e8bd195f
MrBanh <tonybanh@live.com>
2021-01-27 03:49:19 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $firstName = $_GET["firstName"];
+    $lastName = $_GET["lastName"];
+    $age = $_GET["age"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tony Banh - Week 2 Assignment</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+    <h1>Hello</h1>
+</body>
+</html>
